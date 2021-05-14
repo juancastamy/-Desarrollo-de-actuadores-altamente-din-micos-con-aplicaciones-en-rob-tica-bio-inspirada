79fc56138888df9b47e39f8b71fb7a594158f097
juancastamy <45299603+juancastamy@users.noreply.github.com>
2021-05-14 11:02:38 -0600

    inicio de proyecto

---
+++ PWM/Debug/main.d
@@ -0,0 +1,7 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/adc.h
+
+../main.c: 
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/adc.h: 
---
+++ PWM/main.c
@@ -0,0 +1,9 @@
+#include "driverlib/adc.h"
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	return 0;
+}
