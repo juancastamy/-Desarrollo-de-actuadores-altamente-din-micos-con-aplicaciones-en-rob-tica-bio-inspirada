f4ee01ebf7442af5070878d49041e7f0c48a02c8
juancastamy <45299603+juancastamy@users.noreply.github.com>
2021-10-11 14:21:58 -0600

    se inicio

---
+++ PID_cascada/Debug/CONFIG.d
@@ -0,0 +1,63 @@
+# FIXED
+
+CONFIG.obj: ../CONFIG.c
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdint.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdbool.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/string.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+CONFIG.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdarg.h
+CONFIG.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/qei.h
+
+../CONFIG.c: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdbool.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/string.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/qei.h: 
