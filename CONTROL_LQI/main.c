e893228a8d6905902c20b48254b9f1a6a3a87aa7
juancastamy <45299603+juancastamy@users.noreply.github.com>
2021-11-25 19:06:23 -0600

    comentarios

--- CONTROL_LQI/main.c
+++ CONTROL_LQI/main.c
@@ -31,83 +31,58 @@
  * main.c
  */
 
-uint32_t COUNT;
-uint32_t corriente;
+uint32_t corriente;//Valirbale para almacenar el valor ADC de corriente
 //*******************************************************VARIABLE PARA CAMBIO DE GIRO DEL MOTOR***********************************************
 int left;
 int right;
 //******************************************************VARIABLE PARA ALMACENAR EL MENSAJE POR COMUNICACION UART*******************************
-unsigned char data[20];
+unsigned char data[20]; //array donde se guardan los bytes para ser enviados por UART
 
 char n;
 int s;
 
-uint32_t PWM_FREC=40000;
+uint32_t PWM_FREC=40000;//frecuencia a la que trabaja el PWM
 uint32_t pwm_word;
 
-float delta_pos=0;
-float pos_tot=0;
-float pos_pas=0;
+//********************************VARIABLE PARA REALIZAR AJUSTE DE LA POSICION DE ENCODER**************************************************
+float delta_pos = 0;
+float pos_tot = 0;
+float pos_pas = 0;
 float j=0;
 
-float velocidad;
-float posicion;
-float sentido;
-float current;
-float volt_medido;
-
-float ref;
-float pos;
-
-float tau;
-float tau_e;
-
-float error_tau;
-float xI;
-float dt;
-float u;
-/*
-const float k1 = 0.1993;
-const float k2 = -0.0500;
-const float k3 = -0.0808;
-const float k4 = -20.0000;
-*/
-
-/*
-const float k1 = 1;
-const float k2 = 1;
-const float k3 = -1;
-const float k4 = 5;
-*/
-/*
-const float k1 = 2.5;
-const float k2 = 1;
-const float k3 = -1;
-const float k4 = 8;
-*/
-float G=10;
 
 
+float velocidad;//variable para guardar el valor de la velocidad (rad/s)
+float posicion;//variable para guardar el valor leido del encoder
+float sentido;//variable para guardar el sentido en el que gira el motor
+float current;//varaible para guardar la corriente estimada
+float volt_medido;//variable para guardar el voltaje medido en el ADC
+
+float ref_grados;//referencia de la posicion en grados
+float salida_posicion;//salida de la posicion en grados
+
+float tau_e;//valor del torque medido
+
+float xI;//error integral
+float dt;//periodo de muestreo
+float u;//salida del LQI
+
+float G=10;//ganancia para las constantes del controlador
+
+//**************************************************constantes para el control LQI****************************************
 const float k1 = 1;
 const float k2 = 0.05;
 const float k3 = -0.08;
 const float k4 = 5;
 
-//float Ek = 0;
+float pulso;//valor de salida del controlador en un rango de 0 a 4095
 
-float pulso;
-float posible_I;
+float posref = 0.0;//referncia de la posicion
 
-float posref = 0.00;
-
-int update;
+int update;//badnera para la interripcion del TIMER0
 
 int fl;
 int serial;
-
-int BOTON;
-
-struct Filtro pt;
 
 float giro;
 float rev;
@@ -116,35 +91,21 @@ float current_filt;
 float lambda = 0.7;
 float error_pos = 0;
 
-float volts = 0;
-
-float ref22;
-struct Filtro
-{
-    float pot;
-
-};
-
-struct Filtro filtrado(float senal, float S, float alpha)
-{
-    struct Filtro potenciometro;
-    s = (alpha*senal)+((1-alpha)*S);
-    potenciometro.pot = s;
-    return potenciometro;
-}
-
+//**********************************INTERRIPCION DEL TIMER0 PARA LA TOMA DE MUESTRAS******************************
 void Timer0IntHandler(void){
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
     update = 1;
 
 }
 
+//******************************INTERRUPCION PARA REALIZAR LA COMUNICACION UART*********************************
 void UART0IntHandler(void){
     UARTIntClear(UART0_BASE,UARTIntStatus(UART0_BASE, true)); //se limpia la bandera de la interrupcion
     n = UARTCharGet(UART0_BASE);//se lee lo que entra al UART0
     serial=1;
 }
 
+//*****************************FUNCION PARA DECIDIR EL CENTIDO DE GIRO DEL MOTOR*********************************
 void direccion(float dir)
 {
     if(dir > 0 )
@@ -170,58 +131,32 @@ void direccion(float dir)
 int main(void)
 
 {
-    //1.3
-    ref22=0;
-    ref = 0;
     xI = 0;
-    dt = 0.001;
-    pulso = 10;
-    ref = 0;
-    CONFIG();
-    pwm_word = ((SysCtlClockGet()/1)/PWM_FREC)-1;
-    //GPIOPinWrite(GPIO_PORTA_BASE,GPIO_PIN_5,GPIO_PIN_5);//se enciende pin//el encoder sumara
-    //GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6,0x00);//se apaga pin
+    dt = 0.001;//periodo de muestreo
+    CONFIG();//funcion que se llama para realizar las configuraciones del microcontrolador
+    pwm_word = ((SysCtlClockGet()/1)/PWM_FREC)-1;//factor por el que se tiene que multiplicar el valor que se envia al PWM
+
     while(1)
     {
         if(update == 1)
         {
             //-------------------------------------SE LIMPIA BANDERA DE ADC PARA INICIAR A LEER EL VALOR OBTENIDO--------------------------
-           ADCIntClear(ADC0_BASE, 3);
-           ADCProcessorTrigger(ADC0_BASE, 3);//SE ACTIVA EL TRIGUER PARA REALIZAR LECTURA
-           while(!ADCIntStatus(ADC0_BASE, 3, false));//SE ESPERA QUE EL STATUAS DEL ADC SEA FALSE
-           ADCSequenceDataGet(ADC0_BASE, 3, &COUNT);// SE LEE ADC DE LA REFERENCIA
-
            ADCIntClear(ADC1_BASE, 0);
            ADCProcessorTrigger(ADC1_BASE, 0);//SE ACTIVA EL TRIGUER PARA REALIZAR LECTURA
            while(!ADCIntStatus(ADC1_BASE, 0, false));//SE ESPERA QUE EL STATUAS DEL ADC SEA FALSE
            ADCSequenceDataGet(ADC1_BASE, 0, &corriente);// SE LEE ADC DE RETROALIMENTACION DE LA CORRIENTE
 
-           if (fl==0)
-           {
-               fl=1;
-               pt.pot = COUNT;
-           }
-
-           pt = filtrado(COUNT, pt.pot, 0.05);
-
-           //ref = (float)(pt.pot);
-
-           tau = ref * (0.08 / 4095);
-
-           posicion = (float)(QEIPositionGet(QEI0_BASE)*2*3.1416/979.2);//360/979.2);
-           velocidad = (float)(QEIVelocityGet(QEI0_BASE)*100*60/979.2)*0.10472;
-           sentido = QEIDirectionGet(QEI0_BASE);
+           posicion = (float)(QEIPositionGet(QEI0_BASE)*2*3.1416/979.2);//lectura de la posicion en radiantes
+           velocidad = (float)(QEIVelocityGet(QEI0_BASE)*100*60/979.2)*0.10472;//lectura de la velocidad en rad/s
+           sentido = QEIDirectionGet(QEI0_BASE);//direccion en la que gira el motor
 
            volt_medido = (((float)corriente)*3300)/4095;//esto esta en V
            current = (((float)corriente)/4.5232)*(3.3/4095)*(1000/0.144);//esto esta en mA
 
-           current_filt = lambda * current_filt + (1-lambda) * current;
+           current_filt = lambda * current_filt + (1-lambda) * current;//corriente filtrada
 
-           tau_e = current_filt*0.065;
+           tau_e = current_filt*0.065;//torque experimental medido
 
-           //error_tau = tau - tau_e;
-
-           //xI = xI + error_tau*dt;
            //---------------------------------------obtencion de la posicion en valores positivos y negativos--------------------------------------
            delta_pos = posicion - pos_pas;
            pos_pas = posicion;
@@ -262,88 +197,50 @@ int main(void)
                }
            }
 
-
-
-
-           error_pos = -posref + pos_tot;
-           if (error_pos > 0.01 || error_pos < -0.01)
+           error_pos = -posref + pos_tot;//error de posicion
+           if (error_pos > 0.01 || error_pos < -0.01)//tolerancia para el error de integracion
            {
-           xI = xI + error_pos*dt;
+               xI = xI + error_pos*dt;
            }
 
-           //u = -1*((k1*tau_e) + (k2*posicion) + (k3*velocidad) + (20*k4*xI)); no
+          u = -(((G*k1*tau_e)/1000 + (G*k2*pos_tot) + (G*k3*velocidad)) + (G*k4*xI));//salida del controlador
 
-          u = -(((G*k1*tau_e)/1000 + (G*k2*pos_tot) + (G*k3*velocidad)) + (G*k4*xI));//si
+          //u = -0.2*50*(pos_tot - posref);//control de posicion tipo P
 
-
-          //u = -0.2*50*(pos_tot - posref);
-
-          // Ek=(pos_tot - posref);
-
-           update = 0;
+           update = 0;//se limpia la bandera de la interrupcion
         }
 
-
-
-        //direccion(u);//se selecciona la direccion en la que el motor gira
-
-        //pulso = ((float)abs(u)*4095)/12;
-
-        direccion(u);
-        pulso = 30 +((float)abs(u)*4095)/12;
-        //IF PARA REALIZAR LA TOMA DE DATOS CON UART
-        /*  if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)==0)
-        {
-            BOTON = 1;
-        }
-        if(BOTON==1)
-        {
-            ref = 25;
-            GPIOPinWrite(GPIO_PORTA_BASE,GPIO_PIN_5,GPIO_PIN_5);//se apaga pin
-            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6,0x00);//se apaga pin
-        }
-        else
-        {
-            GPIOPinWrite(GPIO_PORTA_BASE,GPIO_PIN_5,0x00);//se apaga pin
-            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6,0x00);//se apaga pin
-        }*/
-        /*if(pt.pot < 30)
-        {
-            ref = 30;
-        }
-        else if (pt.pot > 4055)
-        {
-            ref = 4055;
-        }*/
+        direccion(u);//se decide la direccion del motor
+        pulso = 30 +((float)abs(u)*4095)/12;//se calcula el valor para enviar al PWM
         PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, pulso*pwm_word/4095);//se envia la salida del controlador convertido a señal PWM al pwm.
 
-        ref = posref * 180/3.1416;
-        pos = pos_tot * 180/3.1416;
+        ref_grados = posref * 180/3.1416;//se convierte el valor de la referencia a grados
+        salida_posicion = pos_tot * 180/3.1416;//se convieret a grados el valor leido del encoder
 
 
 //****************************************************PREPARACION PARA ENVIO DE DATOS POSICION**********************************************
-        data[0] = ((uint32_t)ref >> 24) & 0xff;  //se extrae los bits 24-31 de la señal de referencia de posicion y se almacenan en la primera
+        data[0] = ((uint32_t)ref_grados >> 24) & 0xff;  //se extrae los bits 24-31 de la señal de referencia de posicion y se almacenan en la primera
                                                  //posicion del array data
 
-        data[1] = ((uint32_t)ref >> 16) & 0xff;  //se extrae los bits 16-23 de la señal de referencia de posicion y se almacenan en la segunda
+        data[1] = ((uint32_t)ref_grados >> 16) & 0xff;  //se extrae los bits 16-23 de la señal de referencia de posicion y se almacenan en la segunda
                                                  //posicion del array data
 
-        data[2] = ((uint32_t)ref >>  8) & 0xff;  //se extrae los bits 8-15 de la señal de referencia de posicion y se almacenan en la tercera
+        data[2] = ((uint32_t)ref_grados >>  8) & 0xff;  //se extrae los bits 8-15 de la señal de referencia de posicion y se almacenan en la tercera
                                                  //posicion del array data
 
-        data[3] = (uint32_t)ref & 0xff; //se extrae los bits 0-7 de la señal de referencia de posicion y se almacenan en la cuarta
+        data[3] = (uint32_t)ref_grados & 0xff; //se extrae los bits 0-7 de la señal de referencia de posicion y se almacenan en la cuarta
                                         //posicion del array data
 
-        data[4] = ((uint32_t)pos >> 24) & 0xff; //se extrae los bits 24-31 de la señal de posicion del encoder y se almacenan en la quinta
+        data[4] = ((uint32_t)salida_posicion >> 24) & 0xff; //se extrae los bits 24-31 de la señal de posicion del encoder y se almacenan en la quinta
                                                      //posicion del array data
 
-        data[5] = ((uint32_t)pos >> 16) & 0xff; //se extrae los bits 16-23 de la señal de posicion del encoder y se almacenan en la sexta
+        data[5] = ((uint32_t)salida_posicion >> 16) & 0xff; //se extrae los bits 16-23 de la señal de posicion del encoder y se almacenan en la sexta
                                                      //posicion del array data
 
-        data[6] = ((uint32_t)pos >> 8) & 0xff; //se extrae los bits 8-15 de la señal de posicion del encoder y se almacenan en la septima
+        data[6] = ((uint32_t)salida_posicion >> 8) & 0xff; //se extrae los bits 8-15 de la señal de posicion del encoder y se almacenan en la septima
                                                     //posicion del array data
 
-        data[7] = (uint32_t)pos; //se extrae los bits 0-7 de la señal de posicion del encoder y se almacenan en la octava
+        data[7] = (uint32_t)salida_posicion; //se extrae los bits 0-7 de la señal de posicion del encoder y se almacenan en la octava
                                       //posicion del array data
 //*******************************************************ENVIO DATOS VELOCIDAD*********************************************************
         data[8] = ((uint32_t)current >> 24) & 0xff;  //se extrae los bits 24-31 de la señal de referencia de velocidad y se almacenan en la novena
